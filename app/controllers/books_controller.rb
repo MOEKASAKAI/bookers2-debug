54249fcc641ca2f0caf90b0a651732d221a6021d
MOEKASAKAI <wpi.floves2@gmail.com>
2021-03-01 06:32:48 +0000

    [Fix]　ログインユーザ投稿詳細画面のエラー修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,7 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
     @user = @book.user
+    @new_book = Book.new
   end
 
   def index
@@ -37,9 +38,9 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
